553b8cf6a7390f9e5611ad8737f5ae4003993b4f
OsadtsivMykhailo <osadtsiv.m@gmail.com>
2021-09-01 09:31:00 +0300

    lesson 4  / Dependancy Injection (setter+ value(xml)

--- src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -3,6 +3,9 @@ package ru.alishev.springcourse;
 public class MusicPlayer {
     private Music music1;
 
+    private String name;
+    private int volume;
+
     //IoC (Inversion of Control/dependancies)
 //2    public MusicPlayer(Music music2) {
 //2        this.music1 = music2;
@@ -13,6 +16,22 @@ public class MusicPlayer {
 
     public void setMusic1(Music music1) {
         this.music1 = music1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 
     public void playMusic() {
