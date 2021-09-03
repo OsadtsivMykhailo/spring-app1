5b8580d3ac1054e0dece93a6326fea821d3c2174
OsadtsivMykhailo <osadtsiv.m@gmail.com>
2021-09-03 19:03:08 +0300

    lesson 8  / Init, Destroy, Factory methods

--- src/main/java/ru/alishev/springcourse/RockMusic.java
+++ src/main/java/ru/alishev/springcourse/RockMusic.java
@@ -1,6 +1,15 @@
 package ru.alishev.springcourse;
 
 public class RockMusic implements Music{
+
+    public  void doMyInit() {
+        System.out.println("Doing my Rock initialization");
+    }
+
+    public void doMyDestroy() {
+        System.out.println("Doing my Rock destruction");
+    }
+
     @Override
     public String getSong() {
         return "We are the champions";
--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -13,20 +13,22 @@ public class TestSpring {
 
      //1   MusicPlayer musicPlayer = new MusicPlayer(music1);
 
-        MusicPlayer f1MusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        f1MusicPlayer.playMusic();
+//        MusicPlayer f1MusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        f1MusicPlayer.playMusic();
+//
+//        MusicPlayer f2MusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        boolean comp = f1MusicPlayer == f2MusicPlayer;
+//        System.out.println(comp);
+//
+//        f1MusicPlayer.setVolume(20);
+//
+//        System.out.println(f1MusicPlayer.getName());
+//        System.out.println(f1MusicPlayer.getVolume());
+//        System.out.println(f2MusicPlayer.getVolume());
 
-        MusicPlayer f2MusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-
-        boolean comp = f1MusicPlayer == f2MusicPlayer;
-        System.out.println(comp);
-
-        f1MusicPlayer.setVolume(20);
-
-        System.out.println(f1MusicPlayer.getName());
-        System.out.println(f1MusicPlayer.getVolume());
-        System.out.println(f2MusicPlayer.getVolume());
-
+        ClassicalMusic classicalMusic = context.getBean("musicBean1", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
         context.close();
     }
 }
